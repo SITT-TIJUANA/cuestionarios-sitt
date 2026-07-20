48fae960452e68dabadffb15dfd845375cb1c21e
SITT-TIJUANA <gjvazquez@tijuana.gob.mx>
2026-07-20 09:41:01 -0700

    Delete backend directory

--- backend/migracion-respuesta-correcta.sql
+++
@@ -1,3 +0,0 @@
--- Permite marcar la "respuesta correcta" en preguntas de completar, libre y escala
--- (opción múltiple ya usa opciones.es_correcta, no necesita esto)
-ALTER TABLE preguntas ADD COLUMN IF NOT EXISTS respuesta_correcta TEXT;
