ba89d5ab3c5bd2a0e6e5965bad1b65bbbb5f43e5
SITT-TIJUANA <gjvazquez@tijuana.gob.mx>
2026-07-20 09:41:19 -0700

    Delete frontend directory

--- frontend/src/components/BotonSitt.jsx
+++
@@ -1,7 +0,0 @@
-export default function BotonSitt({ children, onClick, disabled, type = 'button' }) {
-  return (
-    <button className="boton-uiverse" onClick={onClick} disabled={disabled} type={type}>
-      {children}
-    </button>
-  );
-}
